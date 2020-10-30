0f67c74857bb96ebda339ad18c5d4415f81fc7e5
CMD <gautamkailashsingh@gmail.com>
2020-10-31 00:31:25 +0530

    [Gautam] Added a test method to test the calculateFare() method.

--- NUnitTestProject1/UnitTest1.cs
+++ NUnitTestProject1/UnitTest1.cs
@@ -10,6 +10,17 @@ namespace NUnitTestProject1
         {
         }
 
+        [Test]
+        public void GivenDistanceAndTimeShouldReturnTotalFare()
+        {
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double distance = 2.0;
+            int time = 5;
+            double fare = invoiceGenerator.CalculateFare(distance, time);
+            double expected = 25;
+            Assert.AreEqual(expected, fare);
+        }
+
         
 
 
