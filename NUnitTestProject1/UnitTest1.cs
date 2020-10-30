bf62daf45d4c69fb1b3b1a3de6e7ee666d297ef0
CMD <gautamkailashsingh@gmail.com>
2020-10-31 00:35:23 +0530

    [Gautam] Added a test method to test calculation of fare for multiple rides.

--- NUnitTestProject1/UnitTest1.cs
+++ NUnitTestProject1/UnitTest1.cs
@@ -21,7 +21,16 @@ namespace NUnitTestProject1
             Assert.AreEqual(expected, fare);
         }
 
-        
+        [Test]
+        public void GivenMultipleRidesShouldReturnInvoiceSummary()
+        {
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            Ride[] rides = { new Ride(2, 5), new Ride(0.1, 1) };
+            InvoiceSummary summary = invoiceGenerator.CalculateFare(rides);
+            InvoiceSummary expectedSummary = new InvoiceSummary(2, 30);
+            Assert.AreEqual(expectedSummary, summary);
+        }
+
 
 
 
